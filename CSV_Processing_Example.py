9a9bf0e3998d6ef3c865a016aa4ebaea6c3f1006
Clarence Cheung <83518886+clarencehycheung@users.noreply.github.com>
2022-02-04 11:16:11 -0700

    Update and rename CSV_Processing_Example to CSV_Processing_Example.py

--- CSV_Processing_Example.py
+++ CSV_Processing_Example.py
@@ -8,22 +8,21 @@ from scipy import optimize
 
 path = askdirectory(title='Select Folder')
 
+# Converting into Dataframe
 df = pd.read_csv(path+'\Trial1.csv')
 voltage = df['Voltage'].values
 current = df['Current'].values
 
+# Linear Fit 
+def Delta(x):
+  return len(x)*np.sum(x**2)-np.sum(x)**2
+def A(x,y):
+  return (np.sum(x**2)*np.sum(y)-np.sum(x)*np.sum(x*y))/Delta(x)
+def B(x,y):
+  return (len(x)*np.sum(x*y)-np.sum(x)*np.sum(y))/Delta(x)
 def myquad(x, a,b,c):
   return a*(x-b)**2+c
 
-databool = (voltage>11.5)*(voltage<14)
-
-popt, pcov = optimize.curve_fit(myquad,
-                                voltage[databool],
-                                current[databool],
-                                [50, 4.5, 40]
-                                )
-
-xtest = np.linspace(11.5, 14, 101)
 plt.errorbar(voltage, current, zorder=1)
 plt.plot(xtest, myquad(xtest, popt[0], popt[1], popt[2]), '--r', zorder=2)
 plt.xlabel('Voltage (V)')
